a4126bd5ec1d4676de09307cccdcc5ffd738821c
Milica <shkordova.milica@gmail.com>
2024-11-04 00:25:25 +0100

    fix: changed the default server into localhost:3000

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dns from "dns";
+
+//changed Vite's default server to use http://localhost:3000 instead of http://127.0.0.1:5173
+dns.setDefaultResultOrder("verbatim");
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    server: {
+        host: "localhost",
+        port: 3000,
+    },
+});
