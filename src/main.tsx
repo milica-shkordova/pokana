8fbd93a626399f3d85d6996af892407cf4dafb20
Milica <shkordova.milica@gmail.com>
2024-11-04 01:41:39 +0100

    feat: installed sass

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./index.scss";
 
 const rootElement = document.getElementById("root");
 
