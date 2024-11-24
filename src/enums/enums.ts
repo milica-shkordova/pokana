4562a3b57925ea6146a2b91888cf27cf76c2f943
Milica <shkordova.milica@gmail.com>
2024-11-25 00:42:05 +0100

    feat: created constants,enums,interfaces,mixins,variables,assets

---
+++ src/enums/enums.ts
@@ -0,0 +1,21 @@
+export enum InputType {
+    EMAIL = "email",
+    PASSWORD = "password",
+    SUBMIT = "submit",
+    TEXT = "text",
+    NUMBER = "number",
+}
+
+export enum ButtonType {
+    PRIMARY = "primary",
+    SECONDARY = "secondary",
+}
+
+export enum TextTag {
+    P = "p",
+    LI = "li",
+    H1 = "h1",
+    H2 = "h2",
+    H3 = "h3",
+    SPAN = "span",
+}
